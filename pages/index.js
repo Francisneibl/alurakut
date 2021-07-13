1942f5a9d5d2e38f370ed16b8736ce0f403de793
Francisneibl <francisneibernardeslima@gmail.com>
2021-07-12 23:39:49 -0300

    inicio alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,69 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.gitHubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
 export default function Home() {
-  return <Title>My page</Title>
+  const gitHubUser = "Francisneibl";
+  const favoritePeople = [
+    "juunegreiros",
+    "omariosouto",
+    "peas",
+    "rafaballerini",
+    "marcobrunodev",
+    "felipefialho",
+  ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSideBar gitHubUser={gitHubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+
+            <OrkutNostalgicIconSet confiavel={3} sexy={2} legal={2} />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade({favoritePeople.length})
+            </h2>
+
+            <ul>
+              {favoritePeople.map((currentItem) => {
+                return (
+                  <li key={currentItem}>
+                    <a href={`users/${currentItem}`}>
+                      <img src={`https://github.com/${currentItem}.png`} />
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
